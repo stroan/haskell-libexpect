4b2480fe3c1aefa3ecb939700cd3a4c4412ad011
Stephen Roantree <stroantree@gmail.com>
2009-11-24 15:58:54 +0000

    added documentation, and fixed libexpect.cabal

--- System/Expect/ExpectInterface.hs
+++ System/Expect/ExpectInterface.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
+-- | Provides a thin wrapper for the FFI bindings to libexpect contained in
+-- System.Expect.ExpectBindings.
 module System.Expect.ExpectInterface 
   ({-- The contents of the binding that are to be further exposed --}
-   ExpCase,expExact,expRegexp,expGlob,expNull
+   ExpectType(ExpExact,ExpRegex,ExpGlob,ExpNull)
    {-- The custom interface --}
-  ,ExpectCase(ExpectCase)
-  ,ExpectProc,expectHandle,expectFilePtr
+  ,ExpectCase(ExpectCase,expectPattern,expectType,expectValue)
+  ,ExpectProc(ExpectProc,expectHandle,expectFilePtr)
   ,muteExpect,unmuteExpect
   ,spawnExpect
-  ,expectCases,expectExact,expectRegex
+  ,expectCases,expectExact,expectRegex,expectMultiple
   ,sendLine)
 where
 
@@ -22,22 +24,42 @@ import IO
 
 foreign import ccall "stdio.h fileno" fileno :: Ptr CFile -> IO CInt
 
-data ExpectCase = ExpectCase { expectPattern ::String
-     		  	     , expectType :: ExpType 
-			     , expectValue :: Int }
+-- * Types
 
-data ExpectProc = ExpectProc { expectFilePtr :: Ptr CFile
+-- | Denotes how a case's pattern is treated.
+data ExpectType = ExpExact  -- ^ Match against pattern exactly.
+                | ExpRegex  -- ^ Compile the pattern string to a regex and match.
+                | ExpGlob   -- ^ Pattern string is glob style.
+                | ExpNull
+
+-- | Defines a case to match against.
+data ExpectCase = ExpectCase { -- | Pattern to match against.
+                               expectPattern ::String 
+                               -- | Type of pattern contained in the case.
+     		  	     , expectType :: ExpectType 
+                               -- | The value to return if the case is matched.
+			     , expectValue :: Int     
+                             }
+
+-- | Proc created by spawnExpect. Contains both the
+-- CFile pointer and a Haskell handle, so the
+-- translation needs only be done once.
+data ExpectProc = ExpectProc { -- | Gets the pointer to the expect process file handle.
+                               expectFilePtr :: Ptr CFile
+                               -- | Gets a Handle to the expect process.
      		  	     , expectHandle :: Handle }
 
+-- | Child process does not echo output to stdout.
 muteExpect :: IO ()
 muteExpect = poke exp_loguser 0
 
+-- | Child process echoes output to stdout.
 unmuteExpect :: IO ()
 unmuteExpect = poke exp_loguser 1
 
--- Spawn a new expect process, running a specified program.
-spawnExpect :: String
-            -> IO ExpectProc
+-- | Spawn a new expect process, running a specified program.
+spawnExpect :: String -- ^ The command to be processed. eg. "adduser bob"
+            -> IO ExpectProc -- ^ Expect process.
 spawnExpect cmd = do
   cstr <- newCString cmd
   cfileptr <- EB.exp_popen cstr
@@ -45,10 +67,13 @@ spawnExpect cmd = do
   handle <- fdToHandle $ fromIntegral cfileno
   return $ ExpectProc cfileptr handle
 
+
+expectCases :: ExpectProc -- ^ The process to expect on.
+            -> [ExpectCase] -- ^ The cases to match against.
+            -> IO (Maybe Int) -- ^ Nothing if there are no matches (timeout / EOF), the value field
+                              -- of the case that matched.
+
 -- Expect one of a list of cases
-expectCases :: ExpectProc
-            -> [ExpectCase]
-            -> IO (Maybe Int)
 expectCases proc cases = do
   scases <- mapM toStorableCase cases
   sarray <- newArray (scases ++ [endStorableCase])
@@ -58,31 +83,36 @@ expectCases proc cases = do
   if cval < 0 then return Nothing
      	      else return $ Just $ fromEnum cval
 
-expectSingle :: ExpectProc
-             -> String
-             -> ExpType
-             -> IO (Maybe Int)
+-- | Expect a single case with a given type.
+expectSingle :: ExpectProc -- ^ The process to expect on.
+             -> String     -- ^ The pattern.
+             -> ExpectType -- ^ The type of the pattern.
+             -> IO (Maybe Int) -- ^ See expectCases.
 expectSingle proc str ec = expectCases proc [ExpectCase str ec 1]
 
-expectExact :: ExpectProc
-            -> String
-            -> IO (Maybe Int)
-expectExact proc exact = expectSingle proc exact expExact
+-- | Expect a single case with a type of ExpExact.
+expectExact :: ExpectProc -- ^ The process to expect on.
+            -> String -- ^ The pattern.
+            -> IO (Maybe Int) -- ^ See expectCases.
+expectExact proc exact = expectSingle proc exact ExpExact
 
-expectRegex :: ExpectProc
-            -> String
-            -> IO (Maybe Int)
-expectRegex proc reg = expectSingle proc reg expRegexp
+-- | Expect a single case with a type of ExpExact.
+expectRegex :: ExpectProc -- ^ The process to expect on.
+            -> String -- ^ The pattern.
+            -> IO (Maybe Int) -- ^ See expectCases.
+expectRegex proc reg = expectSingle proc reg ExpRegex
 
-expectMultiple :: ExpectProc
-               -> [String]
-               -> ExpType
-               -> IO (Maybe Int)
+-- | Expect multiple cases of a given type.
+expectMultiple :: ExpectProc -- ^ The process to expect on.
+               -> [String] -- ^ The patterns.
+               -> ExpectType -- ^ The type of the pattern.
+               -> IO (Maybe Int) -- ^ See expectCases.
 expectMultiple proc ss ec = expectCases proc cases
     where cases = map (\(x,y) -> ExpectCase x ec y) (zip ss [1..])
 
-sendLine :: ExpectProc
-         -> String
+-- | Send a line of input to the process.
+sendLine :: ExpectProc -- ^ The process.
+         -> String -- ^ The line to send, without the '\n'
          -> IO ()
 sendLine proc line = hPutStrLn (expectHandle proc) line
 
@@ -95,7 +125,7 @@ toStorableCase :: ExpectCase
 toStorableCase cs = do
   cstr <- newCString $ expectPattern cs
   cval <- (return . toEnum . expectValue) cs
-  return $ ExpCase cstr nullPtr (expectType cs) cval
+  return $ ExpCase cstr nullPtr (expectTypeToExpType $ expectType cs) cval
 
 endStorableCase :: ExpCase
 endStorableCase = ExpCase nullPtr nullPtr expEnd 0
@@ -105,3 +135,9 @@ freeStorableCase :: ExpCase
 freeStorableCase cs = do
   if (regexp cs) == nullPtr then free (regexp cs)
      	     	    	    else return ()
+
+expectTypeToExpType :: ExpectType -> ExpType
+expectTypeToExpType ExpRegex = expRegexp
+expectTypeToExpType ExpExact = expExact
+expectTypeToExpType ExpGlob = expGlob
+expectTypeToExpType ExpNull = expNull
